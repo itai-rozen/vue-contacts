8f3ff06a2c79cdfe3eb021b74d8d7dd6d975fc62
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-06 14:43:28 +0300

    added SingleContact component with dynamic props from AllContacts

--- client/src/components/SingleContact.vue
+++ client/src/components/SingleContact.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="contact-container">
-    <img :src="contact.image_url" alt="">
-    
-
-    
+    <img :src="contact.image_url" :alt="contact.fullname">
+    <h2>{{contact.fullname}}</h2>
+    <p>nickname: "{{contact.nickname}}"</p>
+    <p>username: {{contact.username}}</p>
+    <p>Email: {{contact.email}}</p>
+    <p>Phone: {{contact.phone}}</p>
+    <p>Address: {{contact.address}}</p>
+    <p>Gender: {{getGender}}</p>
+    <button>info</button>
+    <button>delete</button>
   </div>  
 </template>
 
 <script>
 export default {
+  data(){
+    return {
 
+    }
+  },
+  computed: {
+    getGender(){
+      return this.contact.is_female? '👱‍♀️' : '🧔'
+    }
+  },
   props: {
     contact: {
       id: Number,
-      fullName: String,
-      userName: String,
-      nickName: String,
+      fullname: String,
+      username: String,
+      nickname: String,
       phone: Number,
       email: String,
       additional: String,
