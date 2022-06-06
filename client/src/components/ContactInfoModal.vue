776732c5250b500409d417f0934fd67430d56017
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-06 15:15:23 +0300

    created a contact modal with additional info to be displayed when user clicks the info button

---
+++ client/src/components/ContactInfoModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="contact-modal-container">
+    <div class="contact-modal"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+  .contact-modal-container {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1;
+}
+.contact-modal {
+  height: 70%;
+  width: 80%;
+}
+</style>
