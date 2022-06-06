f265903011261e1ae81de30de7d2233dad554e6b
itai rozen <itai.rozen@mail.huji.ac.il>
2022-06-06 15:14:44 +0300

    single contact cpt renders image according to url status, gender is computed

--- client/src/components/SingleContact.vue
+++ client/src/components/SingleContact.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="contact-container">
-    <img :src="contact.image_url" :alt="contact.fullname">
+    <img :src="getImageSrc" :alt="contact.fullname">
     <h2>{{contact.fullname}}</h2>
     <p>nickname: "{{contact.nickname}}"</p>
     <p>username: {{contact.username}}</p>
@@ -9,20 +9,27 @@
     <p>Address: {{contact.address}}</p>
     <p>Gender: {{getGender}}</p>
     <button>info</button>
-    <button>delete</button>
+    <button @click="deleteContact(contact.id)">delete</button>
   </div>  
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data(){
     return {
-
+      isInfo: false
     }
   },
   computed: {
     getGender(){
       return this.contact.is_female? '👱‍♀️' : '🧔'
+    },
+    getImageSrc(){
+      if (!this.contact.image_url){
+        return (this.isFemale)? 'assets/femaleAvatar.png' : 'assets/maleAvatar.png' 
+      }
+      return this.contact.image_url
     }
   },
   props: {
@@ -37,6 +44,11 @@ export default {
       address: String,
       image_url: String,
       isFemale: Boolean
+    }
+  },
+  methods: {
+    deleteContact: async function(id){
+      await axios.delete(`http://localhost:3001/${id}`)
     }
   }
 }
